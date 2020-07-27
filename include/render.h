6b3ec29a2d70982ca1672ed32d7a8c8a023c8d6b
renehorstmann <r.horsty@gmail.com>
2020-07-27 13:28:24 +0200

    some topdown code ideas...

--- include/render.h
+++ include/render.h
@@ -1,37 +1,14 @@
 #ifndef GYRONAUT_RENDER_H
 #define GYRONAUT_RENDER_H
 
-#include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <SDL2/SDL.h>
 
-static float deg2rad(float deg) {
-    return deg * (float) M_PI / 180.0f;
-}
-static float rad2deg(float rad) {
-    return rad * 180.0f / (float) M_PI;
-}
 
-void render_init(SDL_Renderer *renderer, int screen_w, int screen_h);
+void r_init();
 
-struct rrect {
-    float x, y; // mid point
-    float w, h;
-    float angle;
-};
 
-bool rreect_contains(struct rreect rect, float x, float y);
+void r_render(double delta_time, int wnd_width, int wnd_height);
 
-struct color {
-    Uint8 r, g, b, a;
-};
-
-typedef struct {
-    struct rrect rect;
-    SDL_Texture *tex;
-    SDL_Rect tex_pos;
-    struct color color;
-} renderobject;
-
-void render_objects(renderobject *objects, int n);
 
 #endif //GYRONAUT_RENDER_H
--- src/render.c
+++ src/render.c
@@ -1,59 +1,7 @@
 #include "render.h"
 
-static SDL_Renderer *renderer;
-static int screen_w, screen_h;
-static float screen_w_half, screen_h_half;
-static float screen_scale;
 
-void render_init(SDL_Renderer *r, int w, int h) {
-    renderer = r;
-    screen_w = w;
-    screen_h = h;
-    screen_w_half = (float) w / 2.0f;
-    screen_h_half = (float) h / 2.0f;
-    screen_scale = screen_h < screen_w ? (float) screen_h / 100.0f : (float) screen_w / 100.0f;
-}
-
-static SDL_Rect rrect_to_SDL_Rect(struct rrect rect) {
-    SDL_Rect res;
-    res.x = (int) (screen_w_half + rect.x * screen_scale);
-    res.y = (int) (screen_h_half + rect.y * screen_scale);
-    res.w = (int) (rect.w * screen_scale);
-    res.h = (int) (rect.h * screen_scale);
-    return res;
-}
-
-bool rrect_contains(struct rrect rect, float x, float y) {
-    float dx = x - rect.x;
-    float dy = y - rect.y;
-
-    float alpha = atan2f(dy, dx);
-    float hyp = dx / cosf(alpha);
-
-    float rotated_x = hyp * cosf(alpha - rect.angle);
-    float rotated_y = hyp * sinf(alpha - rect.angle);
-
-    return rotated_x < rect.w / 2 && rotated_x > -rect.w / 2
-           && rotated_y < rect.h / 2 && rotated_y > -rect.h / 2;
-}
-
-void render_objects(renderobject *objects, int n) {
-    for(int i=0; i<n; i++) {
-        SDL_Rect rect = rrect_to_SDL_Rect(objects[i].rect);
-
-        SDL_SetRenderDrawColor(renderer,
-                objects[i].color.r,
-                objects[i].color.g,
-                objects[i].color.b,
-                objects[i].color.a);
-
-        SDL_RenderCopyEx(renderer,
-                         objects[i].tex,
-                         &objects[i].tex_pos,
-                         &rect,
-                         rad2deg(objects[i].rect.angle),
-                         NULL,
-                         SDL_FLIP_NONE);
-    }
-}
+#define GL_GLEXT_PROTOTYPES
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
