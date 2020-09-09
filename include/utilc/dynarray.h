2aa1ffcfa983a1aaf03b734dc06afc3e7da5b045
renehorstmann <r.horsty@gmail.com>
2020-09-09 12:18:45 +0200

    utilc update

--- include/utilc/dynarray.h
+++ include/utilc/dynarray.h
@@ -6,28 +6,28 @@
 #define DynArray_INIT {0}
 
 /**
- * Creates an dynamic array of type (type) with the typename name.
+ * Creates an dynamic array of type (type) with the typename class_name.
  * Construct it as empty struct ( = {};, or calloc).
- * Kill it with <name>_kill;
- * <name>_set_capacity sets the internal capacity. If realloc fails, nothing changes.
- * <name>_resize sets the array size and may set capacity with size*2.
- * <name>_push copies an item to the end of the array (copies to the last element).
- * <name>_append creates an item at the end and returns its pointer (returns the last element).
- * <name>_pop returns a copy of the last item and removes it from the array.
+ * Kill it with <fn_name>_kill;
+ * <fn_name>_set_capacity sets the internal capacity. If realloc fails, nothing changes.
+ * <fn_name>_resize sets the array size and may set capacity with size*2.
+ * <fn_name>_push copies an item to the end of the array (copies to the last element).
+ * <fn_name>_append creates an item at the end and returns its pointer (returns the last element).
+ * <fn_name>_pop returns a copy of the last item and removes it from the array.
  */
-#define DynArray(type, name) \
-typedef struct name { \
+#define DynArray(type, class_name, fn_name) \
+typedef struct class_name { \
     type *array; \
     size_t capacity; \
     size_t size; \
-} name; \
-static void name ## _kill(name *self) { \
+} class_name; \
+static void fn_name ## _kill(class_name *self) { \
     free(self->array); \
     self->array = NULL; \
     self->capacity = 0; \
     self->size = 0; \
 } \
-static void name ## _set_capacity(name *self, size_t capacity) { \
+static void fn_name ## _set_capacity(class_name *self, size_t capacity) { \
     type *new_array = (type *) realloc(self->array, capacity * sizeof(type)); \
     if (new_array) { \
         self->array = new_array; \
@@ -36,29 +36,29 @@ static void name ## _set_capacity(name *self, size_t capacity) { \
             self->size = capacity; \
     } \
 } \
-static void name ## _resize(name *self, size_t size) { \
+static void fn_name ## _resize(class_name *self, size_t size) { \
     if (size > self->capacity) { \
-        name ## _set_capacity(self, size * 2); \
+        fn_name ## _set_capacity(self, size * 2); \
         if(size>self->capacity) \
             /* alloc error */ \
             return;\
     } \
     self->size = size; \
 } \
-static void name ## _push(name *self, type item) { \
-    name ## _resize(self, self->size+1); \
+static void fn_name ## _push(class_name *self, type item) { \
+    fn_name ## _resize(self, self->size+1); \
     self->array[self->size-1] = item; \
 } \
-static void name ## _push_array(name *self, const type *item_array, size_t n) { \
-    name ## _resize(self, self->size+n); \
+static void fn_name ## _push_array(class_name *self, const type *item_array, size_t n) { \
+    fn_name ## _resize(self, self->size+n); \
     for(int i=0; i<n; i++)\
         self->array[self->size-n+i] = item_array[i]; \
 } \
-static type *name ## _append(name *self) { \
-    name ## _resize(self, self->size+1); \
+static type *fn_name ## _append(class_name *self) { \
+    fn_name ## _resize(self, self->size+1); \
     return &self->array[self->size-1]; \
 } \
-static type name ## _pop(name *self) { \
+static type fn_name ## _pop(class_name *self) { \
     if(self->size > 0) \
         return self->array[--self->size]; \
     return (type) {0}; \
@@ -68,28 +68,28 @@ static type name ## _pop(name *self) { \
 
 
 /**
- * Creates an dynamic array of type (type) with the typename name.
+ * Creates an dynamic array of type (type) with the typename class_name.
  * This version dont uses copies (push and pop). For use with array types like int[2].
  *
  * Construct it as empty struct ( = {};, or calloc).
- * Kill it with <name>_kill;
- * <name>_set_capacity sets the internal capacity. If realloc fails, nothing changes.
- * <name>_resize sets the array size and may set capacity with size*2.
- * <name>_append creates an item at the end and returns its pointer (returns the last element).
+ * Kill it with <fn_name>_kill;
+ * <fn_name>_set_capacity sets the internal capacity. If realloc fails, nothing changes.
+ * <fn_name>_resize sets the array size and may set capacity with size*2.
+ * <fn_name>_append creates an item at the end and returns its pointer (returns the last element).
  */
-#define DynArrayWithoutCopy(type, name) \
-typedef struct name { \
+#define DynArrayWithoutCopy(type, class_name, fn_name) \
+typedef struct class_name { \
     type *array; \
     size_t capacity; \
     size_t size; \
-} name; \
-static void name ## _kill(name *self) { \
+} class_name; \
+static void fn_name ## _kill(class_name *self) { \
     free(self->array); \
     self->array = NULL; \
     self->capacity = 0; \
     self->size = 0; \
 } \
-static void name ## _set_capacity(name *self, size_t capacity) { \
+static void fn_name ## _set_capacity(class_name *self, size_t capacity) { \
     type *new_array = (type *) realloc(self->array, capacity * sizeof(type)); \
     if (new_array) { \
         self->array = new_array; \
@@ -98,17 +98,17 @@ static void name ## _set_capacity(name *self, size_t capacity) { \
             self->size = capacity; \
     } \
 } \
-static void name ## _resize(name *self, size_t size) { \
+static void fn_name ## _resize(class_name *self, size_t size) { \
     if (size > self->capacity) { \
-        name ## _set_capacity(self, size * 2); \
+        fn_name ## _set_capacity(self, size * 2); \
         if(size>self->capacity) \
             /* alloc error */ \
             return;\
     } \
     self->size = size; \
 } \
-static type *name ## _append(name *self) { \
-    name ## _resize(self, self->size+1); \
+static type *fn_name ## _append(class_name *self) { \
+    fn_name ## _resize(self, self->size+1); \
     return &self->array[self->size-1]; \
 }
 
--- include/utilc/hashmap.h
+++ include/utilc/hashmap.h
@@ -6,43 +6,43 @@
 #include <string.h>
 
 /**
- * Creates a HashMap with the name (name) and the types for key and value (key_type) (value_type).
+ * Creates a HashMap with the name (class_name) and the types for key and value (key_type) (value_type).
  * The function hashfunction should take a key and return an unsigned hash number.
  * The function keycmp_function should return true, if both key parameters are equal.
  * The function keycpy_function should copy the input key into the return value.
  * The function keykill_function should free the given key.
  * 
- * Construct a Map with <name> map; <name>_new(&map, approx_size);.
- * Kill it with <name>_kill.
- * <name>_get returns an pointer to a value for the given key (if not available yet, it will create a new entry).
- * <name>_remove removes the value under the given key, if available.
+ * Construct a Map with <class_name> map; <fn_name>_new(&map, approx_size);.
+ * Kill it with <fn_name>_kill.
+ * <fn_name>_get returns an pointer to a value for the given key (if not available yet, it will create a new entry).
+ * <fn_name>_remove removes the value under the given key, if available.
  *
- * Get a map iterator with <name>_get_iter(&map);
+ * Get a map iterator with <fn_name>_get_iter(&map);
  */
-#define HashMap(name, key_type, value_type, hash_function, keycmp_function, keycpy_function, keykill_function)\
-typedef struct name ## _Item {\
+#define HashMap(key_type, value_type, class_name, fn_name, hash_function, keycmp_function, keycpy_function, keykill_function)\
+typedef struct class_name ## _Item {\
     key_type key;\
     value_type value;\
-    struct name ## _Item *next;\
-} name ## _Item;\
-typedef struct name {\
-    name ## _Item **map;\
+    struct class_name ## _Item *next;\
+} class_name ## _Item;\
+typedef struct class_name {\
+    class_name ## _Item **map;\
     int map_size;\
-} name;\
-typedef struct name ## _Iter {\
-    name *hashmap;\
+} class_name;\
+typedef struct class_name ## _Iter {\
+    class_name *hashmap;\
     int hash_row;\
-    name ## _Item *next;\
-} name ## _Iter;\
-void name ## _new(name *self, int approx_size) {\
-    self->map = (name ## _Item **) calloc(approx_size, sizeof(name ## _Item *));\
+    class_name ## _Item *next;\
+} class_name ## _Iter;\
+void fn_name ## _new(class_name *self, int approx_size) {\
+    self->map = (class_name ## _Item **) calloc(approx_size, sizeof(class_name ## _Item *));\
     self->map_size = approx_size;\
 }\
-void name ## _kill(name  *self) {\
+void fn_name ## _kill(class_name  *self) {\
     for (int i = 0; i < self->map_size; i++) {\
-        name ## _Item *item = self->map[i];\
+        class_name ## _Item *item = self->map[i];\
         while (item) {\
-            name ## _Item *next = item->next;\
+            class_name ## _Item *next = item->next;\
             keykill_function(item->key);\
             free(item);\
             item = next;\
@@ -52,35 +52,35 @@ void name ## _kill(name  *self) {\
     self->map = NULL;\
     self->map_size = 0;\
 }\
-value_type *name ## _get(const name *self, key_type key) {\
+value_type *fn_name ## _get(const class_name *self, key_type key) {\
     unsigned hash = hash_function(key) % self->map_size;\
-    name ## _Item **item = &self->map[hash];\
+    class_name ## _Item **item = &self->map[hash];\
     while (*item && !keycmp_function(key, (*item)->key))\
         item = &(*item)->next;\
     if (!*item) {\
-        *item = (name ## _Item *) malloc(sizeof(name ## _Item));\
+        *item = (class_name ## _Item *) malloc(sizeof(class_name ## _Item));\
         (*item)->key = keycpy_function(key);\
         (*item)->next = NULL;\
     }\
     return &(*item)->value;\
 }\
-void name ## _remove(const name *self, key_type key) {\
+void fn_name ## _remove(const class_name *self, key_type key) {\
     unsigned hash = hash_function(key) % self->map_size;\
-    name ## _Item **item = &self->map[hash];\
+    class_name ## _Item **item = &self->map[hash];\
     while (*item && !keycmp_function(key, (*item)->key))\
         item = &(*item)->next;\
     if (*item) {\
-        name ## _Item *kill = *item;\
+        class_name ## _Item *kill = *item;\
         *item = (*item)->next;\
         keykill_function(kill->key);\
         free(kill);\
     }\
 }\
-name ## _Iter name ## _get_iter(name *self) {\
-    return (name ## _Iter) {self, -1, NULL};\
+class_name ## _Iter fn_name ## _get_iter(class_name *self) {\
+    return (class_name ## _Iter) {self, -1, NULL};\
 }\
-name ## _Item *name ## _Iter_next(name ## _Iter *it) {\
-    name ## _Item *ret;\
+class_name ## _Item *fn_name ## _Iter_next(class_name ## _Iter *it) {\
+    class_name ## _Item *ret;\
     if(it->next) {\
         ret = it->next;\
         it->next = ret->next;\
@@ -124,14 +124,14 @@ static void StrHashMap_impl_keykill(const char *key) {
 
 
 /**
- * Creates a string HashMap with the name (name) and the type for value (value_type).
+ * Creates a string HashMap with the name (class_name) and the type for value (value_type).
  * 
- * Construct a Map with <name> map; <name>_new(&map, approx_size);.
- * Kill it with <name>_kill.
- * <name>_get returns an pointer to a value for the given string key (if not available yet, it will create a new entry).
- * <name>_remove removes the value under the given string key, if available.
+ * Construct a Map with <class_name> map; <fn_name>_new(&map, approx_size);.
+ * Kill it with <fn_name>_kill.
+ * <fn_name>_get returns an pointer to a value for the given string key (if not available yet, it will create a new entry).
+ * <fn_name>_remove removes the value under the given string key, if available.
  */
-#define StrHashMap(name, value_type) HashMap(name, const char *, value_type,\
+#define StrHashMap(value_type, class_name, fn_name) HashMap(const char *, value_type, class_name, fn_name,\
 StrHashMap_impl_hash, StrHashMap_impl_keycmp, StrHashMap_impl_keycpy, StrHashMap_impl_keykill)
 
 
